6e08d79c0873743dbdc61b63fbcad1af44c8dd74
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2024-04-16 15:21:51 +0200

    add / completes themes (capitals, continents, currencies, religions)

--- src/theme/get-theme.tsx
+++ src/theme/get-theme.tsx
@@ -2,9 +2,18 @@ import capitals from "../data/capitals.json";
 import currencies from "../data/currencies.json";
 import languages from "../data/laguages.json";
 import baby from "../data/baby.json";
+import continents from "../data/continents.json";
+import religions from "../data/religions.json";
 import { ThemeType } from "../types";
 export const getThemeList = (): string[] => {
-  return ["Baby", "Capitals", "Currencies", "Languages"];
+  return [
+    "Baby",
+    "Capitals",
+    "Currencies",
+    "Languages",
+    "Continents",
+    "Religions",
+  ];
 };
 export const getTheme = (name?: string): ThemeType => {
   switch (name) {
@@ -19,6 +28,12 @@ export const getTheme = (name?: string): ThemeType => {
       break;
     case "Baby":
       return baby;
+      break;
+    case "Continents":
+      return continents;
+      break;
+    case "Religions":
+      return religions;
       break;
     default:
       return baby;
